93d8161b0631907914dda95bbb756acd5e9e2841
Selamawit <ezanawittadele@gmail.com>
2024-04-03 14:13:05 -0700

    simple ruby on rail crud api

---
+++ app/controllers/api/v1/articles_controller.rb
@@ -0,0 +1,76 @@
+class Api::V1::ArticlesController < ApplicationController
+  def index
+    articles=Article.all
+
+    render json:articles,status:200
+  end
+
+  def show
+
+    article=Article.find_by(id: params[:id])
+
+    if article
+      render json:article,status:200
+    else
+      render json:{
+        error:"article not found"
+      }
+    end
+
+
+
+  end
+
+  def create
+    article = Article.new(
+      title: arti_params[:title],
+      body: arti_params[:body],
+      author: arti_params[:author]
+    )
+
+    if article.save
+      render json: article, status: 200
+    else
+      render json: { error: "error occurred" }
+    end
+  end
+
+  def update
+
+    article=Article.find_by(id: params[:id])
+
+    if article
+      article.update(title:params[:title],body:params[:body],author:params[:author])
+      render json:article,status:200
+    else
+      render json:{
+        error:"article not found"
+      }
+
+    end
+
+  end
+
+  def destroy
+
+    article=Article.find_by(id: params[:id])
+
+    if article
+      article.destroy
+      render json:"article deleted successfuly"
+
+    end
+
+
+  end
+
+  private
+
+  def arti_params
+    params.require(:article).permit([
+      :title,
+      :body,
+      :author
+    ])
+  end
+end
